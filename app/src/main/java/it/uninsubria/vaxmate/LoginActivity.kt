d1810d3029ba691060b70ed03c063621806dc0b3
Cele-stino <celestino.resteghini@gmail.com>
2026-04-23 18:37:25 +0200

    Modifice su LoginActivity

--- app/src/main/java/it/uninsubria/vaxmate/LoginActivity.kt
+++ app/src/main/java/it/uninsubria/vaxmate/LoginActivity.kt
@@ -5,6 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.UnderlineSpan
+import android.view.View
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +24,46 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvFooter = findViewById<TextView>(R.id.tvFooter)
+
+        val text = "Continuando, accetti i Termini di Servizio e la Privacy Policy di VaxMate."
+        val spannable = SpannableString(text)
+
+        val termsText = "Termini di Servizio"
+        val privacyText = "Privacy Policy"
+
+        val termsStart = text.indexOf(termsText)
+        val termsEnd = termsStart + termsText.length
+
+        val privacyStart = text.indexOf(privacyText)
+        val privacyEnd = privacyStart + privacyText.length
+
+        // CLICK TERMS
+        spannable.setSpan(object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                // TODO: apri schermata Termini
+                // startActivity(Intent(this@LoginActivity, TermsActivity::class.java))
+            }
+        }, termsStart, termsEnd, 0)
+
+        // CLICK PRIVACY
+        spannable.setSpan(object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                // TODO: apri schermata Privacy
+                // startActivity(Intent(this@LoginActivity, PrivacyActivity::class.java))
+            }
+        }, privacyStart, privacyEnd, 0)
+
+        // stile (blu + underline)
+        spannable.setSpan(ForegroundColorSpan(Color.BLUE), termsStart, termsEnd, 0)
+        spannable.setSpan(UnderlineSpan(), termsStart, termsEnd, 0)
+
+        spannable.setSpan(ForegroundColorSpan(Color.BLUE), privacyStart, privacyEnd, 0)
+        spannable.setSpan(UnderlineSpan(), privacyStart, privacyEnd, 0)
+
+        tvFooter.text = spannable
+        tvFooter.movementMethod = LinkMovementMethod.getInstance()
+        tvFooter.highlightColor = Color.TRANSPARENT
     }
 }
